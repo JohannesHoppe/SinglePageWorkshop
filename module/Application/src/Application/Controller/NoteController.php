d7e3187e819f6542e576928c6b340383e0d355f4
Artimon <webmaster@pad-soft.de>
2013-11-08 18:26:57 +0100

    Initial commit, page with prepared API controller.

---
+++ module/Application/src/Application/Controller/NoteController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractRestfulController;
+use Zend\View\Model\JsonModel;
+
+class NoteController extends AbstractRestfulController {
+	/**
+	 * @return array|mixed
+	 */
+	public function getList() {
+		// @TODO Insert get all code and return data. -Artimon
+
+		return array('data' => array(
+			array('id' => 1, 'title' => 'Hallo'),
+			array('id' => 2, 'title' => 'Du'),
+		));
+	}
+
+	/**
+	 * @param mixed $id
+	 * @return array|mixed
+	 */
+	public function get($id) {
+		// @TODO Insert get code and return data. -Artimon
+
+		return array('data' => array('id' => 1, 'title' => 'Hallo'));
+	}
+
+	/**
+	 * @param mixed $data
+	 * @return int|mixed
+	 */
+	public function create($data) {
+		// @TODO Insert create code and return data by new id. -Artimon
+		$newId = 3;
+
+		return new JsonModel(array(
+			$this->get($newId)
+		));
+	}
+
+	/**
+	 * @param mixed $id
+	 * @param mixed $data
+	 * @return mixed|JsonModel
+	 */
+	public function update($id, $data) {
+		// @TODO Add update code here. -Artimon
+
+		return new JsonModel(array(
+			$this->get($id)
+		));
+	}
+
+	/**
+	 * @param mixed $id
+	 * @return mixed|JsonModel
+	 */
+	public function delete($id) {
+		// @TODO Add delete code here. -Artimon
+
+		return new JsonModel(array(
+			'data' => 'deleted'
+		));
+	}
+}
