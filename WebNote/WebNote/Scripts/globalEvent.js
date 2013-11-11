8bbf7fac5b3af29f7e166a61b3ff35320a2ce117
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2013-11-11 23:14:14 +0100

    adding .net code

---
+++ WebNote/WebNote/Scripts/globalEvent.js
@@ -0,0 +1,16 @@
+/**
+ * Global events can be used to trigger events, and pass event-data, across frameworks.
+ */
+define(['jquery'], function ($) {
+
+    var $eventobject = $({});
+
+    return {
+        bind: function () {
+            $eventobject.bind.apply($eventobject, arguments);
+        },
+        trigger: function () {
+            $eventobject.trigger.apply($eventobject, arguments);
+        }
+    };
+});
