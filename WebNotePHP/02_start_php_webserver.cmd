11dbeb94e25d226d8c3b2bc2193df416caf8637f
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2013-11-13 00:30:11 +0100

    finishing empty versions

--- WebNotePHP/02_start_php_webserver.cmd
+++ WebNotePHP/02_start_php_webserver.cmd
@@ -6,12 +6,12 @@ CD "%~dp0"
 echo Uses PHP 5.5 built-in web server to serve a Zend Framework application.
 echo PHP.exe needs to be in your PATH
 
-start http://localhost:1337
+start http://localhost:1339
 
 SET APPLICATION_ENV=development
 SET PHPRC=%~dp0vendor\php-win\
 
 cd public
-php -S localhost:1337 -d extension_dir=%PHPRC%
+php -S localhost:1339 -d extension_dir=%PHPRC%
 
 pause
